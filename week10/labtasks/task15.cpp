3a94f5b27b32eb66163ac93228c000b11866a9d9
MuhammadUzair323 <ranauzair32303@gmail.com>
2026-04-28 21:24:01 +0500

    Week 10 tasks

--- week10/labtasks/task15.cpp
+++ week10/labtasks/task15.cpp
@@ -1,48 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-string convertToEnglish(int num1){
-int number;
-    number=num1%10;
 
-    string first, second;
-    if(number==0){second="zero";}
-    if(number==1){second="one";}
-    if(number==2){second="two";}
-    if(number==3){second="three";}
-    if(number==4){second="four";}
-    if(number==5){second="five";}
-    if(number==6){second="six";}
-    if(number==7){second="seven";}
-    if(number==8){second="eight";}
-    if(number==9){second="nine";}
+string calculatePoolState(float V,float P1,float P2,float H);
 
-    if(num1==10){second="ten";}
-    if(num1==11){second="eleven";}
-    if(num1==12){second="twelve";}
-    if(num1==13){second="thirteen";}
-    if(num1==14){second="fourteen";}
-    if(num1==15){second="fifteen";}
-    if(num1==16){second="sixteen";}
-    if(num1==17){second="seventeen";}
-    if(num1==18){second="eighteen";}
-    if(num1==19){second="nineteen";}
-
-    if(num1>=20&&num1<30){first="twenty";}
-    if(num1>=30&&num1<40){first="thirty";}
-    if(num1>=40&&num1<50){first="forty";}
-    if(num1>=50&&num1<60){first="fifty";}
-    if(num1>=60&&num1<70){first="sixty";}
-    if(num1>=70&&num1<80){first="seventy";}
-    if(num1>=80&&num1<90){first="eighty";}
-    if(num1>=90&&num1<100){first="ninety";}
-
-    if(num1==100){first="hundered";}
-    return first +" "+ second;
-
+main()
+{
+float V;
+cout<<"Enter volume of the pool in liters: ";
+cin>>V;
+float P1;
+cout<<"Enter flow rate of the first pipe per hour: ";
+cin>>P1;
+float P2;
+cout<<"Enter flow rate of the second pipe per hour: ";
+cin>>P2;
+float H;
+cout<<"Enter hours that the worker is absent: ";
+cin>>H;
+    string result = calculatePoolState(V,P1,P2,H);
+    cout << result;
 }
-int main(){
-    int number;
-    cout<<"Enter a number: ";
-    cin>>number;
-    cout<<convertToEnglish(number);
+
+string calculatePoolState(float V,float P1,float P2,float H)
+{
+    int t_water = (P1+P2)*H;
+    
+    int pool_per, pipe1_contri, pipe2_contri;
+    if(t_water<=V){
+    pool_per = (t_water/V)*100;
+    pipe1_contri = ((P1*H)/t_water)*100;
+    pipe2_contri = ((P2*H)/t_water)*100;
+
+    return "The pool is "+to_string(pool_per)+"% full. Pipe 1: "+to_string(pipe1_contri)+"%. Pipe 2: "+to_string(pipe2_contri)+"%.";
+}else{
+    int overflow;
+    overflow = t_water-V;
+    return "For "+to_string(H)+" hours, the pool overflows with : "+to_string(overflow)+" liters.";
+}
+
 }
